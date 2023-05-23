e789a8b77082300d1c9fc0d04a4d0cf900452eef
David Stainton <dstainton415@gmail.com>
2023-05-23 04:34:45 -0400

    Cleanup fillrandom test

--- common_test.go
+++ common_test.go
@@ -5,14 +5,25 @@ import (
 	"testing"
 
 	gopointer "github.com/mattn/go-pointer"
+	"github.com/stretchr/testify/require"
 )
 
 func TestFillRandom(t *testing.T) {
 	message := []byte("AAAA")
 	rng := bytes.NewReader(message)
 	p := gopointer.Save(rng)
-	outsz := 4
+	outsz := len(message)
 	out := test_c_buf(outsz)
 	test_go_fillrandom(p, out, outsz)
 	t.Logf("out: `%s`", test_GoString(out, outsz))
+	require.Equal(t, message, []byte(test_GoString(out, outsz)))
+
+	message = []byte("how now brown cow")
+	rng = bytes.NewReader(message)
+	p = gopointer.Save(rng)
+	outsz = len(message)
+	out = test_c_buf(outsz)
+	test_go_fillrandom(p, out, outsz)
+	t.Logf("out: `%s`", test_GoString(out, outsz))
+	require.Equal(t, message, []byte(test_GoString(out, outsz)))
 }
